167ae29955294c67683d32aba02af0491bff747f
isdanchuk bc2025-2 <lizadanchuk943@gmail.com>
2025-10-07 01:50:30 +0300

    Оновлено part3.js та urls.txt для варіанту 2

--- part3.js
+++ part3.js
@@ -1,19 +1,13 @@
-// part3.js
 const { URL, URLSearchParams } = require('url');
 
-// Константи для варіанта 2 (Доходи та витрати банків)
-const date = "202408";       // серпень 2024 (формат YYYYMM)
-const time_period = "month"; // період - місяць
+const date = "20240801";
+const period = "month";
 
-// Базовий URL API НБУ для доходів та витрат банків
 const nbuUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/banksincexp");
-
-// Додаємо параметри запиту через searchParams
 nbuUrl.search = new URLSearchParams({
-  json: "",          // формат JSON
   date: date,
-  period: time_period
+  period: period,
+  json: ""
 });
 
-// Виводимо готовий URL у консоль
-console.log("Сконструйований URL для частини 2:", nbuUrl.toString());
+console.log(nbuUrl.toString());
